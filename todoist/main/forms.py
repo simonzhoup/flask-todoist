98406f6399fbb5ccbd0ec56bb90b974b87e086f5
simonzhoup <simonzhoup@outlook.com>
2018-01-08 20:40:51 +0800

    精简前端代码，优化业务逻辑

--- todoist/main/forms.py
+++ todoist/main/forms.py
@@ -1,7 +1,8 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SubmitField, BooleanField, SelectField, DateTimeField, DateField
 from wtforms.validators import Required, Length, Optional
 from flask_login import current_user
+from datetime import datetime
 
 
 class AddProject(FlaskForm):
@@ -11,13 +12,15 @@ class AddProject(FlaskForm):
 
 class AddTask(FlaskForm):
     task = StringField('任务：', validators=[Required(), Length(1, 64)])
-    title = SelectField('标签：', validators=[Optional()])
-    new_title = StringField('新标签：', validators=[Optional(), Length(1, 8)])
-    project = SelectField('项目:')
-    timenode = StringField('时间节点：')
+    title = SelectField('标签：', render_kw={"style": "width:20%;"})
+    new_title = StringField('新标签：', validators=[Optional(), Length(
+        1, 32)], render_kw={"style": "width:20%;"})
+    project = SelectField('项目:', render_kw={"style": "width:20%;"})
+    timenode = StringField('时间节点：', render_kw={
+                           "style": "width:20%;"}, default=datetime.now().strftime("%Y-%m-%d"))
     priority = SelectField(
-        '优先级：', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4')])
-    tasksave = SubmitField('保存')
+        '优先级：', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4')], render_kw={"style": "width:20%;"})
+    submit = SubmitField('保存')
 
     def __init__(self, *args, **kwargs):
         super(AddTask, self).__init__(*args, **kwargs)
@@ -27,19 +30,7 @@ class AddTask(FlaskForm):
                               for t in current_user.titles]
 
 
-class EditTask(FlaskForm):
-    task = StringField('任务：', validators=[Required(), Length(1, 64)])
-    project = SelectField('项目：')
-    # title = SelectField('标签：')
-    # new_title = StringField('新标签：')
-    timenode = StringField('时间节点：')
-    priority = SelectField(
-        '优先级：', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4')])
-    tasksave = SubmitField('保存')
-
-    def __init__(self, *args, **kwargs):
-        super(EditTask, self).__init__(*args, **kwargs)
-        self.project.choices = [(p.name, p.name)
-                                for p in current_user.projects]
-        # self.title.choices = [(t.title, t.id)
-        #                       for t in current_user.titles]
+class Search(FlaskForm):
+    s = StringField('', validators=[Required(),
+                                    Length(1, 64)], id="search")
+    search = SubmitField('提交')
--- todoist/main/views.py
+++ todoist/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, session, redirect, url_for, request, abort, make_response
 from datetime import datetime, timezone, timedelta
 from flask_login import login_required, current_user
-from .forms import AddProject, AddTask, EditTask
+from .forms import AddProject, AddTask, Search
 
 from . import main
 from .. import db
@@ -12,56 +12,78 @@ from collections import OrderedDict
 
 @main.before_request
 def main_form():
-    taskform = AddTask()
+    if current_user.is_authenticated:
+        projectform = AddProject()
+        if projectform.validate_on_submit():
+            p = Project(name=projectform.name.data, user_id=current_user.id)
+            db.session.add(p)
+            return redirect(url_for('.index'))
+
+
+@main.route('/search', methods=['GET', 'POST'])
+@login_required
+def search():
+    search = Search()
+    edit_task_form = AddTask()
     projectform = AddProject()
-    if projectform.validate_on_submit():
-        p = Project(name=projectform.name.data, user_id=current_user.id)
-        db.session.add(p)
+    if not search.s.data:
         return redirect(url_for('.index'))
-    if taskform.validate_on_submit():
+    key = '%' + search.s.data + '%'
+    tasks = Task.query.filter(Task.task.like(key)).all()
+    return render_template('tasks-from-project.html', tasks=tasks, key=search.s.data, search=search, edit_task_form=edit_task_form, projectform=projectform)
+
+
+@main.route('/add-task', methods=['GET', 'POST'])
+@login_required
+def add_task():
+    edit_task_form = AddTask()
+    if edit_task_form.validate_on_submit():
         t = Task(task=taskform.task.data, project=taskform.project.data,
                  timenode=taskform.timenode.data, user_id=current_user.id)
-        nt = Titles(title=taskform.new_title.data,
-                    user_id=current_user.id)
-        db.session.add(nt)
-        db.session.commit()
-        t.title = nt.id
-        db.session.add(t)
-        return redirect(url_for('.index'))
+        if taskform.new_title.data:
+            nt = Titles(title=taskform.new_title.data,
+                        user_id=current_user.id)
+            db.session.add(nt)
+            db.session.commit()
+            t.title = nt.id
+            db.session.add(t)
+        else:
+            t.title = taskform.new_title.data
+            db.session.add(t)
+        return redirect(request.referrer)
 
 
 @main.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
     now = datetime.now()
-    taskform = AddTask()
+    search = Search()
+    edit_task_form = AddTask()
     projectform = AddProject()
-    editform = EditTask()
-    taskform.timenode.data = (
-        datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
     today_tasks = current_user.tasks.filter(
         Task.timenode == datetime.now().strftime("%Y-%m-%d")).all()
     overdue_tasks = current_user.tasks.filter(and_(Task.filish == False,
                                                    Task.timenode < datetime.now().strftime("%Y-%m-%d"))).all()
-    return render_template('base.html', now=now, editform=editform, today_tasks=today_tasks, taskform=taskform, projectform=projectform, overdue_tasks=overdue_tasks)
+    return render_template('today.html', now=now, search=search, today_tasks=today_tasks, edit_task_form=edit_task_form, projectform=projectform, overdue_tasks=overdue_tasks)
 
 
 @main.route('/sevenday', methods=['GET', 'POST'])
 @login_required
 def sevenday():
-    taskform = AddTask()
+    search = Search()
+    edit_task_form = AddTask()
     projectform = AddProject()
     days = []
     for i in range(7):
         days.append((datetime.now() + timedelta(days=i)
                      ).strftime("%Y-%m-%d"))
-    tasks = OrderedDict()
+    tasks7 = OrderedDict()
     for day in days:
-        tasks[day] = []
-        for task in current_user.tasks:
-            if task.timenode.split(' ')[0] == day:
-                tasks[day].append(task)
-    return render_template('seven-day.html', tasks=tasks, taskform=taskform, projectform=projectform)
+        tasks7[day] = []
+        for task in current_user.tasks.all():
+            if task.timenode == day:
+                tasks7[day].append(task)
+    return render_template('seven-day.html', search=search, tasks7=tasks7, edit_task_form=edit_task_form, projectform=projectform)
 
 
 @main.route('/task/filish/<int:id>')
@@ -86,9 +108,12 @@ def task_unfilish(id):
 @login_required
 def edit_task(id):
     task = Task.query.get_or_404(id)
+    if request.method == 'GET':
+        return redirect(url_for('.index'))
     if request.method == 'POST':
         task.task = request.form.get('task_task' + str(id))
-        task.project = request.form.get('project' + str(id))
+        if request.form.get('project' + str(id)):
+            task.project = request.form.get('project' + str(id))
         task.timenode = request.form.get('timenode' + str(id))
         task.priority = request.form.get('priority' + str(id))
         db.session.add(task)
@@ -103,16 +128,17 @@ def project(project):
         abort(404)
     tasks = Task.query.filter_by(
         project=project).filter_by(filish=False).all()
-    taskform = AddTask()
+    edit_task_form = AddTask()
     projectform = AddProject()
-    return render_template('tasks-from-project.html', taskform=taskform, projectform=projectform, tasks=tasks, project=project, page_p=project)
+    search = Search()
+    return render_template('tasks-from-project.html', search=search, edit_task_form=edit_task_form, projectform=projectform, tasks=tasks, project=project, page_p=project)
 
 
 @main.route('/title')
 @login_required
 def show_title():
     resp = make_response(redirect(request.referrer))
-    resp.set_cookie('show_what', 'title', max_age=60)
+    resp.set_cookie('show_what', 'title', max_age=60 * 60 * 24 * 30)
     return resp
 
 
@@ -120,7 +146,7 @@ def show_title():
 @login_required
 def show_project():
     resp = make_response(redirect(request.referrer))
-    resp.set_cookie('show_what', 'project', max_age=60)
+    resp.set_cookie('show_what', 'project', max_age=60 * 60 * 24 * 30)
     return resp
 
 
@@ -131,6 +157,7 @@ def title(title):
     if not t:
         abort(404)
     tasks = Task.query.filter_by(title=t.id).filter_by(filish=False).all()
-    taskform = AddTask()
+    edit_task_form = AddTask()
     projectform = AddProject()
-    return render_template('tasks-from-project.html', tasks=tasks, taskform=taskform, projectform=projectform, title=title)
+    search = Search()
+    return render_template('tasks-from-project.html', search=search, tasks=tasks, edit_task_form=edit_task_form, projectform=projectform, title=title)
